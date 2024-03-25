ca22be538663dfc0cf519f1caf960991b191ea01
Артур Захаркин <zaharkin.artur75@gmail.com>
2024-03-25 22:05:18 +0700

    Lab2

---
+++ Models/Proxy.cs
@@ -0,0 +1,56 @@
+﻿using MudBlazor;
+
+namespace OOAP1.Models
+{
+    public interface IElectronicOpt
+    {
+         ElectronicBase? OrderGood();
+    }
+
+    public class ElectronicOpt:IElectronicOpt
+    {
+         public  ElectronicBase? OrderGood()
+        {
+            ElectronicBase result = null;
+
+            switch(new Random().Next(1, 4)) 
+            {
+                case 1: result = new CPU();
+                        break;
+                case 2: result = new HDD();
+                    break;
+                case 3: result = new Motherboard();
+                    break;
+            
+            }
+            return result;
+        }
+    }
+    public class ELectronicOptProxy : IElectronicOpt
+    {
+        private readonly ElectronicOpt electronicOpt;
+
+        public ISnackbar _snackbar;
+
+        private readonly string _role;
+        public ELectronicOptProxy(ElectronicOpt electronicOpt, ISnackbar snackbar, string role)
+        {
+            this.electronicOpt = electronicOpt;
+            this._snackbar = snackbar;          
+            _role = role;
+        }
+        public ElectronicBase? OrderGood()
+        {
+            if(_role.ToLower() == "кладовщик")
+            {
+                _snackbar.Add("Заках создан");
+                return electronicOpt.OrderGood();
+            }
+            else
+            {
+                _snackbar.Add("У вашей роли недостаточно прав");
+                return null;
+            }
+        }
+    }
+}
